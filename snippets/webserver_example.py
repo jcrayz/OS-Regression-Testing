566607aaec3e5ae9952937cc4fe4c02bffaf4794
Christopher Chen <christoipher.chen@gmail.com>
2017-12-01 17:55:17 -0800

    Create webserver example.

---
+++ snippets/webserver_example.py
@@ -0,0 +1,26 @@
+# http://flask.pocoo.org/docs/0.12/quickstart/
+
+from flask import Flask
+import time
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    with open('data.txt') as f:
+        data = f.read()
+    return """
+    <p>
+        Hello, World!
+    </p>
+    <p>
+        The time is currently {}
+    </p>
+    <p>
+        The contents of data.txt are: {}
+    </p>
+    """.format(time.ctime(), data)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
