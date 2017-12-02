0f34eba1b632e6cbbac8fae16e4da5c03eb09cd2
Christopher Chen <christopherdavid.chen@calbaptist.edu>
2017-12-02 14:48:37 -0800

    Deleted

--- snippets/database_example.py
+++ snippets/database_example.py
@@ -9,14 +9,14 @@ c = conn.cursor()
 try:
     c.execute('''CREATE TABLE logs
              (date text, body text)''')
-except sqlite3.OperationalError: #table already exists
+except sqlite3.OperationalError:  # table already exists
     pass
 
 
 if __name__ == '__main__':
     c.execute("INSERT INTO logs VALUES (?,?)",
               (time.time(),
-              ''.join([random.choice(printable) for i in range(30)])))
+               ''.join([random.choice(printable) for i in range(30)])))
     conn.commit()
     results = c.execute('SELECT * FROM logs')
     for r in results:
