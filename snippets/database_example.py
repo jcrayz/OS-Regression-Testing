75bdd56269144fe00b236ac1c5545e6ae9ab74e9
Christopher Chen <christoipher.chen@gmail.com>
2017-12-01 18:11:46 -0800

    Create sqlite example.

---
+++ snippets/database_example.py
@@ -0,0 +1,23 @@
+# https://docs.python.org/2/library/sqlite3.html
+
+import sqlite3
+from string import printable
+import random
+import time
+conn = sqlite3.connect('example.db')
+c = conn.cursor()
+try:
+    c.execute('''CREATE TABLE logs
+             (date text, body text)''')
+except sqlite3.OperationalError: #table already exists
+    pass
+
+
+if __name__ == '__main__':
+    c.execute("INSERT INTO logs VALUES (?,?)",
+              (time.time(),
+              ''.join([random.choice(printable) for i in range(30)])))
+    conn.commit()
+    results = c.execute('SELECT * FROM logs')
+    for r in results:
+        print(r)
