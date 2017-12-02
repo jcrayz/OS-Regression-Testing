ce31c93add158be9116e7bf233995e8ed69236f6
Christopher Chen <christoipher.chen@gmail.com>
2017-12-01 18:13:03 -0800

    Comments

--- snippets/webserver_example.py
+++ snippets/webserver_example.py
@@ -4,11 +4,13 @@ from flask import Flask
 import time
 app = Flask(__name__)
 
-
+# pass url that should map to this function
 @app.route('/')
 def hello_world():
+    # read arbitrary file
     with open('data.txt') as f:
         data = f.read()
+    #     construct a string of output
     return """
     <p>
         Hello, World!
