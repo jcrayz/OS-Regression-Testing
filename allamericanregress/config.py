e93e61a85065c0fa9a7428f6d63cf3822b936471
Christopher Chen <christopher.chen1995@gmail.com>
2018-02-06 14:08:26 -0800

    Remove vestigial hand coded SQL definitions.

--- allamericanregress/config.py
+++ allamericanregress/config.py
@@ -23,60 +23,6 @@ DB_PATH = os.path.join(CONFIG_PATH, 'aar_db.db')
 # Absolute path for log file.
 LOG_PATH = os.path.join(CONFIG_PATH, 'logs.log')
 
-# ========== SQL ==========
-# SQLite table schemas and semantic names.
-DB_TABLES = {
-    'programs': '''CREATE TABLE programs
-                (
-                    id INTEGER PRIMARY KEY,
-                    name TEXT,
-                    path TEXT,
-                    command TEXT
-                );''',
-    'logs': '''CREATE TABLE logs
-                (
-                    program_id INTEGER,
-                    date TEXT,
-                    output TEXT,
-                    exit_code INTEGER,
-                    FOREIGN KEY(program_id) REFERENCES programs(id)
-                );''',
-    'registrants': '''CREATE TABLE registrants
-                (
-                    id INTEGER PRIMARY KEY,
-                    filepath TEXT,
-                    command TEXT,
-                    name TEXT,
-                    author TEXT,
-                    timestamp INTEGER
-                );''',
-    'execution_records': '''CREATE TABLE execution_records
-                (
-                    id INTEGER PRIMARY KEY,
-                    os_version TEXT,
-                    timestamp INTEGER
-                );''',
-    'current_records': '''CREATE TABLE current_records
-                (
-                    registrant_id INTEGER PRIMARY KEY,
-                    last_execution_id INTEGER,
-                    last_successful_execution_id INTEGER,
-                    FOREIGN KEY(registrant_id) REFERENCES registrants(id),
-                    FOREIGN KEY(last_execution_id) REFERENCES execution_records(id),
-                    FOREIGN KEY(last_successful_execution_id) REFERENCES execution_records(id)
-                );''',
-    'failure_records': '''CREATE TABLE failure_records
-                (
-                    registrant_id INTEGER,
-                    execution_id INTEGER,
-                    exit_code INTEGER,
-                    message TEXT,
-                    PRIMARY KEY (registrant_id, execution_id),
-                    FOREIGN KEY(registrant_id) REFERENCES registrants(id),
-                    FOREIGN KEY(execution_id) REFERENCES execution_records(id)
-                );''',
-}
-
 # ========== Logging ==========
 # Ensure the installation directory exists
 if not os.path.isdir(CONFIG_PATH):
