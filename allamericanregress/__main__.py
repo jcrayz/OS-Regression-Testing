c029372b6a601d619aa28ffba42ba7afbc8bb8d6
Christopher Chen <christopher.chen1995@gmail.com>
2018-03-12 20:08:36 -0700

    Make easier to execute as script

--- allamericanregress/__main__.py
+++ allamericanregress/__main__.py
@@ -1,4 +1,10 @@
 import allamericanregress
+import os
+import sys
+# add the modiule location to PATH
+# this is necessary to execute the package as a script without installing it
+to_append = os.path.dirname(os.path.dirname(__file__))
+sys.path.append(to_append)
 
 if __name__ == '__main__':
     try:
