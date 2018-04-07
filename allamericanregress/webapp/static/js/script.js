f26140b1151391d1601d954aebfbcfd25ccdff55
jcrayz <jennal.cray@calbaptist.edu>
2018-04-07 09:19:32 -0700

    button to hide passing tests

--- allamericanregress/webapp/static/js/script.js
+++ allamericanregress/webapp/static/js/script.js
@@ -1,3 +1,6 @@
+/**
+ * Activates the pill in the navigation bar based on the URL
+ */
 function setActiveTab() {
     var page = window.location.pathname;
     var logs_pill = "logs_pill";
@@ -10,5 +13,27 @@ function setActiveTab() {
     } else {
         document.getElementById(home_pill).classList.add('active');
         document.getElementById(logs_pill).classList.remove('active');
+    }
+}
+
+/**
+ * Finds all elements classed as "pass_row" and alternately hides and displays them
+ */
+function togglePassingTests() {
+    var passing_rows = document.getElementsByClassName("tr-pass");
+    var toggle_btn = document.getElementById("toggle-pass");
+    var hide_msg = "Hide Passing Tests";
+    var show_msg = "Show Passing Tests";
+    if (toggle_btn.innerHTML == hide_msg) {
+        toggle_btn.innerHTML = show_msg;
+    } else {
+        toggle_btn.innerHTML = hide_msg;
+    }
+    for (var i = 0; i < passing_rows.length; i++) {
+        if (passing_rows[i].style.display == "none") {
+            passing_rows[i].style.display = "table-row";
+        } else {
+            passing_rows[i].style.display = "none";
+        }
     }
 }
