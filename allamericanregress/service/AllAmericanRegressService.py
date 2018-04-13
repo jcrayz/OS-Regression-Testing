d03c6716b88dce19523a460d85c2675a056e94dc
Christopher Chen <christopher.chen1995@gmail.com>
2018-04-12 22:57:09 -0700

    RIP me

--- allamericanregress/service/AllAmericanRegressService.py
+++ allamericanregress/service/AllAmericanRegressService.py
@@ -2,6 +2,7 @@
 import win32serviceutil
 import win32service
 import win32event
+import win32timezone
 import servicemanager
 import socket
 import time
@@ -14,6 +15,7 @@ import win32com.shell.shell as shell
 import shlex
 
 import sys
+import contextlib
 
 to_append = os.path.dirname(os.path.dirname(__file__))
 sys.path.append(to_append)
@@ -36,12 +38,79 @@ logger = logging.getLogger(__name__)
 logger.info('Service imported')
 
 
+def execute_tests():
+    """Execute all tests from DB."""
+    logger.info('Service is executing tests.')
+    try:
+        if not FROZEN:
+            logger.info('* * Locate pipenv command * *')
+            # child = subprocess.Popen(['pipenv','run','allamericanregress','--execute-tests'],cwd=os.path.dirname(os.path.dirname(os.path.dirname(__file__))), stdout=subprocess.PIPE)
+            child = subprocess.Popen(
+                ['where', 'pipenv'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            # wait for it to finish get an exit code, and get text output
+            console_output = child.communicate()
+            code = child.returncode
+            logger.info('console output: %s', console_output)
+            # ========== Try to install pipenv env ==========
+            logger.info('* * pipenv install * *')
+            # TODO: Use Frozen path
+            env_path = os.path.dirname(
+                os.path.dirname(os.path.dirname(__file__)))
+            logger.info('path=%s', env_path)
+            cmd = shlex.split('python -m pipenv install')
+            logger.info('cmd=%s', cmd)
+            child = subprocess.Popen(
+                cmd, cwd=env_path, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            console_output = child.communicate()
+            code = child.returncode
+            logger.info('pipenv: code=%s, output=%s',
+                        code, console_output)
+            # ========== Execute tests ==========
+            logger.info('* * Executing tests * *')
+            logger.info('execute tests via console')
+            cmd = shlex.split(
+                'python -m pipenv run python -m allamericanregress --execute-tests')
+            logger.info('cmd=%s', cmd)
+            child = subprocess.Popen(
+                cmd, cwd=env_path, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+            console_output = child.communicate()
+            code = child.returncode
+            logger.info('pipenv: code=%s, output=%s',
+                        code, console_output)
+            # logger.info('execute tests via lib')
+        else:
+            logger.info('Execute tests as frozen dist')
+
+            args = ([os.path.join(os.path.dirname(
+                    os.path.abspath(sys.executable)), 'regros', 'regros.exe'), '--execute-tests'],)
+            # args = (['dir'],)
+            kwargs = dict(
+                stdout=subprocess.PIPE,
+                stderr=subprocess.PIPE,
+                stdin=subprocess.PIPE,
+            )
+            logger.info(
+                'Calling regros.exe with args: args=%s kwargs=%s', args, kwargs)
+            # logger.info('Files in dir. cwd=%s files=%s', kwargs[
+            #             'cwd'], os.listdir(kwargs['cwd']))
+            child = subprocess.Popen(*args, **kwargs)
+            console_output = child.communicate()
+            code = child.returncode
+            logger.info('Tests output: %s', console_output)
+
+    except Exception as e:
+        logger.exception(str(e))
+        raise e
+
+
 class RegrOSService(win32serviceutil.ServiceFramework):
     """Creates a Windows service that executes the testing framework automatically on boot."""
     _svc_name_ = "regrOS"
     _svc_display_name_ = "regrOS"  # display name that appears in Service Manager
 
     def __init__(self, args):
+        super().__init__(self, args)
         win32serviceutil.ServiceFramework.__init__(self, args)
         self.stop_event = win32event.CreateEvent(None, 0, 0, None)
         socket.setdefaulttimeout(60)
@@ -55,14 +124,6 @@ class RegrOSService(win32serviceutil.ServiceFramework):
         logger.info('Stopping service ...')
         self.stop_requested = True
 
-    def SvcRun(self):
-        self.ReportServiceStatus(win32service.SERVICE_RUNNING)
-        self.SvcDoRun()
-        # Once SvcDoRun terminates, the service has stopped.
-        # We tell the SCM the service is still stopping - the C framework
-        # will automatically tell the SCM it has stopped when this returns.
-        self.ReportServiceStatus(win32service.SERVICE_STOP_PENDING)
-
     def SvcDoRun(self):
         """When launched by service control manager, log brief intro"""
         logger.info('SvcDoRun')
@@ -72,7 +133,7 @@ class RegrOSService(win32serviceutil.ServiceFramework):
         self.main()
         rc = None
         while rc != win32event.WAIT_OBJECT_0:
-            rc = win32event.WaitForSingleObject(self.hWaitStop, 5000)
+            rc = win32event.WaitForSingleObject(self.stop_event, 5000)
 
     def main(self):
         """Executes the tests registered with the application if OS version change detected"""
@@ -85,7 +146,7 @@ class RegrOSService(win32serviceutil.ServiceFramework):
         #     logger.info(' ** Version changed from {old} to {new}. Running tests.'
         #                  .format(old=last_tested_version, new=current_version))
         logger.info(' ** Executing tests ** ')
-        self.execute_tests()
+        execute_tests()
         # time.sleep(30)
         return
 
@@ -95,92 +156,59 @@ class RegrOSService(win32serviceutil.ServiceFramework):
         version = platform.platform()
         return version
 
-    def execute_tests(self):
-        """Execute all tests from DB."""
-        logger.info('Service is executing tests.')
-        try:
-            if not FROZEN:
-                logger.info('* * Locate pipenv command * *')
-                # child = subprocess.Popen(['pipenv','run','allamericanregress','--execute-tests'],cwd=os.path.dirname(os.path.dirname(os.path.dirname(__file__))), stdout=subprocess.PIPE)
-                child = subprocess.Popen(
-                    ['where', 'pipenv'], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-                # wait for it to finish get an exit code, and get text output
-                console_output = child.communicate()
-                code = child.returncode
-                logger.info('console output: %s', console_output)
-                # ========== Try to install pipenv env ==========
-                logger.info('* * pipenv install * *')
-                # TODO: Use Frozen path
-                env_path = os.path.dirname(
-                    os.path.dirname(os.path.dirname(__file__)))
-                logger.info('path=%s', env_path)
-                cmd = shlex.split('python -m pipenv install')
-                logger.info('cmd=%s', cmd)
-                child = subprocess.Popen(
-                    cmd, cwd=env_path, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-                console_output = child.communicate()
-                code = child.returncode
-                logger.info('pipenv: code=%s, output=%s',
-                            code, console_output)
-                # ========== Execute tests ==========
-                logger.info('* * Executing tests * *')
-                logger.info('execute tests via console')
-                cmd = shlex.split(
-                    'python -m pipenv run python -m allamericanregress --execute-tests')
-                logger.info('cmd=%s', cmd)
-                child = subprocess.Popen(
-                    cmd, cwd=env_path, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-
-                console_output = child.communicate()
-                code = child.returncode
-                logger.info('pipenv: code=%s, output=%s',
-                            code, console_output)
-                # logger.info('execute tests via lib')
-            else:
-                logger.info('Constructing args')
-                args = (['regros.exe', '--execute-tests'],)
-                kwargs = dict(cwd=sys._MEIPASS,
-                              stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-                logger.info(
-                    'Service is frozen and executing with args=%s, kwargs=%s', (args, kwargs))
-                child = subprocess.Popen(*args, **kwargs)
-                console_output = child.communicate()
-                code = child.returncode
-                logger.info('Tests output: %s', console_output)
-
-        except Exception as e:
-            logger.exception(str(e))
-            raise e
-
 
 def install_commandline():
     logger.info('main()-admin Service is admin')
     rs = []
-    logger.info('first win32serviceutil.HandleCommandLine')
-    c = win32serviceutil.HandleCommandLine(RegrOSService, None,
-                                           ["regrOS", "--startup=auto", "install"])
-    logger.info('second win32serviceutil.HandleCommandLine')
-    rs.append(c)
-    # this function behaves as expected if called with "debug" instead of start
-    c = win32serviceutil.HandleCommandLine(
-        RegrOSService, None, ["regrOS", "start"])
-    rs.append(c)
-    logger.info('finished win32serviceutil.HandleCommandLine commands')
-    logger.info(
-        'Service win32serviceutil.HandleCommandLine return codes: %s', rs)
-    logger.info('Process did not die while installing')
+    try:
+        logger.info('first win32serviceutil.HandleCommandLine')
+        c = win32serviceutil.HandleCommandLine(RegrOSService, None,
+                                               ["regrOS", "--startup=auto", "install"])
+        logger.info('second win32serviceutil.HandleCommandLine')
+        rs.append(c)
+        # this function behaves as expected if called with "debug" instead of
+        # start
+        c = win32serviceutil.HandleCommandLine(
+            RegrOSService, None, ["regrOS", "start"])
+        rs.append(c)
+        logger.info('finished win32serviceutil.HandleCommandLine commands')
+        logger.info(
+            'Service win32serviceutil.HandleCommandLine return codes: %s', rs)
+        logger.info('Process did not die while installing')
+    except SystemExit:
+        logger.info('Process died while installing')
 
 
 def main():
     """Installs the service using admin privileges. Privilege code taken from Jorenko's answer at
     https://stackoverflow.com/questions/130763/request-uac-elevation-from-within-a-python-script#answer-11746382"""
+    with open(LOG_PATH, 'a') as f:
+        with contextlib.redirect_stdout(f):
+            win32serviceutil.HandleCommandLine(RegrOSService)
+    quit()
     if FROZEN:
         logger.info('Service main running as frozen dist.')
     else:
         logger.info('Service main running as source dist.')
+    logger.info('Service called with args: %s', sys.argv)
+
+    # win32serviceutil.HandleCommandLine(RegrOSService,argv=sys.argv)
     logger.disabled = False
+    # use this cmd line option in roder to get usage() output
+    # to check if args are invalid
+    ISDRYRUN = "--dryrun" in sys.argv
+    if ISDRYRUN:
+        sys.argv.remove('--dryrun')
+        win32serviceutil.HandleCommandLine(RegrOSService, argv=sys.argv)
     ASADMIN = "--asadmin"
     ISFROZEN = "--frozen"
+    DOINSTALL = "--install"
+
+    # if len(sys.argv) <= 1:
+    #     win32serviceutil.HandleCommandLine(RegrOSService)
+    if len(sys.argv) == 1:
+        execute_tests()
+
     if (ASADMIN not in sys.argv):
         logger.info('Service is not admin')
         script = os.path.abspath(sys.argv[0])  # get current execution command
@@ -200,7 +228,18 @@ def main():
         logger.info('ShellExecuteEx returned this: %s', type(val['hProcess']))
 
     else:
-        install_commandline()
+        logger.info('Service is admin')
+        sys.argv.remove(ASADMIN)
+        logger.info('Delegate to win32serviceutil.HandleCommandLine')
+        with open(LOG_PATH, 'a') as f:
+            with contextlib.redirect_stdout(f):
+                win32serviceutil.HandleCommandLine(
+                    RegrOSService, None, sys.argv)
+
+        # if DOINSTALL in sys.argv:
+        #     logger.info('Installing service')
+        #     sys.argv.remove(DOINSTALL)
+        #     install_commandline()
 
 
 if __name__ == '__main__':
